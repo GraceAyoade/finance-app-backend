44ccbbd6410f242f6876b4907710c0edd8d83f41
graceayoade <tomilewagrace1@gmail.com>
2024-11-20 08:52:42 -0800

    changes made

---
+++ src/controllers/income.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction} from "express";
+
+const logIncome = async(req: Request, res: Response):Promise<void>=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
