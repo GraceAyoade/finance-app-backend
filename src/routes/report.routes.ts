8e39a698932a9839be1ce56a44481dcac59e1427
graceayoade <tomilewagrace1@gmail.com>
2024-11-28 09:16:50 -0800

    backend completed

--- src/routes/report.routes.ts
+++ src/routes/report.routes.ts
@@ -1,13 +1,20 @@
 import express from "express";
-import { getCategorySpending, getExpenseBreakdown, getMonthlyCashFlow, getSummary } from "../controllers/reportController";
+import {
+  getCashFlow,
+  getCategorySpending,
+  getExpenseBreakdown,
+  getMonthlyCashFlow,
+  getSummary,
+} from "../controllers/reportController";
 import authMiddleware from "../middleware/auth.mw";
 
 const reportRouter = express.Router();
 
 reportRouter.use(authMiddleware);
-reportRouter.get("/category-spending", getCategorySpending);
-reportRouter.get("/monthly-cashflow", getMonthlyCashFlow);
+reportRouter.get("/", getCategorySpending);
 reportRouter.get("/summary", getSummary);
+reportRouter.get("/monthly-flow", getMonthlyCashFlow);
+reportRouter.get("/cash-flow", getCashFlow)
 reportRouter.get("/expense-breakdown", getExpenseBreakdown);
 
 export default reportRouter;
